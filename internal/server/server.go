6d6000ce599f9e1183d675faede38e0523eb7013
Madison <maddy08@inbox.ru>
2025-09-04 10:44:00 +0300

    implemented package server.go

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,35 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+type Server struct {
+	logger     *log.Logger
+	httpServer *http.Server
+}
+
+func NewServer(logger *log.Logger, httpServer *http.Server) *Server {
+	router := http.NewServeMux()
+	router.HandleFunc("/", handlers.ServeIndexHTMLHandler)
+	router.HandleFunc("/upload", handlers.UploadHandler)
+
+	httpServer = &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+	return &Server{logger, httpServer}
+}
+
+func (s *Server) Start() error {
+	s.logger.Println("Starting server on port 8080", s.httpServer.Addr)
+	return s.httpServer.ListenAndServe()
+}
